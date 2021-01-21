86898cd7004ec6ea9692f32637eb4a7920f671e8
Simon Busch <simon.busch@live.fr>
2021-01-21 09:31:18 +0100

    details

--- app/javascript/plugins/type.js
+++ app/javascript/plugins/type.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Hello world", "I'm Simon"],
+    strings: ["Hello world", "I'm Simon", "Fullstack web developper"],
     typeSpeed: 150,
     loop: true
   });
